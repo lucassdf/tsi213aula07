035ae68b79f6b388555e04d863b22621cffd71dc
Fábio Abenza <fabiohenriqueaf@users.noreply.github.com>
2021-09-24 18:38:18 -0300

    Versão inicial do projeto

---
+++ app/src/main/java/br/senac/aula07examples/toolbarmenu/OtherActivity.kt
@@ -0,0 +1,12 @@
+package br.senac.aula07examples.toolbarmenu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula07examples.R
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
