f6cc07c64dac22b2ffb4340e00964e2fc3390d08
lucassdf <lucas.souto.freitas@gmail.com>
2021-09-30 23:43:12 -0300

    aula 7 de pdm ate 2h21min

--- app/src/main/java/br/senac/aula07examples/bottomnavigation/BottomNavigationActivity.kt
+++ app/src/main/java/br/senac/aula07examples/bottomnavigation/BottomNavigationActivity.kt
@@ -2,7 +2,11 @@ package br.senac.aula07examples.bottomnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula07examples.R
 import br.senac.aula07examples.databinding.ActivityBottomNavigationBinding
+import br.senac.aula07examples.fragments.AlbumsFragment
+import br.senac.aula07examples.fragments.ArtistsFragment
+import br.senac.aula07examples.fragments.RecentsFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
@@ -12,6 +16,35 @@ class BottomNavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val frag = RecentsFragment.newInstance()
+        supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+
+                R.id.recentes -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+
+                R.id.albuns -> {
+                    val frag = AlbumsFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+
+                R.id.artistas -> {
+                    val frag = ArtistsFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+
+            }
+
+            true
+        }
+
+
+
 
     }
 
