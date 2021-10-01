d22cb46834c0f51383714cd4ceb9fbfec84f5905
lucassdf <lucas.souto.freitas@gmail.com>
2021-10-01 00:36:51 -0300

    aula 7 de pdm final

--- app/src/main/java/br/senac/aula07examples/nv/NavigationViewActivity.kt
+++ app/src/main/java/br/senac/aula07examples/nv/NavigationViewActivity.kt
@@ -1,17 +1,44 @@
 package br.senac.aula07examples.nv
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula07examples.R
 import br.senac.aula07examples.databinding.ActivityNvBinding
+import br.senac.aula07examples.fragments.RecentsFragment
 
 class NavigationViewActivity : AppCompatActivity() {
     lateinit var binding: ActivityNvBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNvBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open_menu, R.string.close_menu)
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationView.setNavigationItemSelectedListener {
+
+            binding.drawerLayout.closeDrawers()
+
+            if(it.itemId == R.id.recentes){
+                val frag = RecentsFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+            }
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return toggle.onOptionsItemSelected(item)
     }
 
 }
